fb856c055e998a74f239af15405ea6fffb46f803
Sivaraj <sivaraj.nagaraj@brillio.com>
2017-11-20 19:42:04 +0530

    materlize sass declarion and basic page declarations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { LeftPaneComponent } from './left-pane/left-pane.component';
+import { RightPaneComponent } from './right-pane/right-pane.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HomeComponent } from './home/home.component';
+import { SkillsComponent } from './skills/skills.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    LeftPaneComponent,
+    RightPaneComponent,
+    NavBarComponent,
+    HomeComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/left-pane/left-pane.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-left-pane',
+  templateUrl: './left-pane.component.html',
+  styleUrls: ['./left-pane.component.scss']
+})
+export class LeftPaneComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
